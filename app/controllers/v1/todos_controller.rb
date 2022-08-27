e2f747cf1c3959428c80e18f87a7b305d1bdfc13
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-08-27 19:43:16 +0900

    chore: 既存のcreateアクションの処理を一旦コメントアウト

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -5,12 +5,14 @@ class V1::TodosController < ApplicationController
   end
 
   def create
-    todo = Todo.new(todo_params)
-    if todo.save
-      render json: todo
-    else
-      rendor json: todo.errors
-    end
+    # todo = Todo.new(todo_params)
+    # if todo.save
+    #   render json: todo
+    # else
+    #   rendor json: todo.errors
+    # end
+
+    puts params
   end
 
   def destroy
