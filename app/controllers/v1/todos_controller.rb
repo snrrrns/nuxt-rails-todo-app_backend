d97b06d700d256b57b8bcbebeccf23dc1e1f790b
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2022-08-28 21:32:38 +0900

    fix: todos_constrollerのcreateアクションを修正

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -5,14 +5,12 @@ class V1::TodosController < ApplicationController
   end
 
   def create
-    # todo = Todo.new(todo_params)
-    # if todo.save
-    #   render json: todo
-    # else
-    #   rendor json: todo.errors
-    # end
-
-    puts params
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo
+    else
+      rendor json: todo.errors
+    end
   end
 
   def destroy
